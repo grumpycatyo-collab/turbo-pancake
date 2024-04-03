39d2b99c7bc05a67a6526c606c14b5b9ddf1d04c
Plamadeala Max <linkolnmax24@gmail.com>
2024-04-03 19:27:02 +0300

    fix minor issue

--- sql_queries/selects/select_non_linked_campaigns.sql
+++ sql_queries/selects/select_non_linked_campaigns.sql
@@ -1,11 +1,4 @@
-SELECT
-    c.id,
-    c.name
-FROM
-    campaigns c
-        LEFT JOIN
-    source_campaign sc
-    ON
-            c.id = sc.campaign_id
-WHERE
-    sc.source_id IS NULL;
+SELECT c.name AS campaign_name
+FROM campaigns c
+         LEFT JOIN source_campaign sc ON c.id = sc.campaign_id
+WHERE sc.source_id IS NULL;
