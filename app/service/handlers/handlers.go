5f2228af934be3c443f08992345dcbea7a024ab7
Plamadeala Max <linkolnmax24@gmail.com>
2024-04-20 15:30:23 +0300

    fix: solved a debugger bug

--- app/service/handlers/handlers.go
+++ app/service/handlers/handlers.go
@@ -8,7 +8,8 @@ import (
 	"github.com/grumpycatyo-collab/turbo-pancake/business/web/mid/cache"
 	"github.com/jmoiron/sqlx"
 	"github.com/rs/zerolog"
-	"github.com/valyala/fasthttp/pprofhandler"
+	"github.com/valyala/fasthttp/fasthttpadaptor"
+	"net/http/pprof"
 	"time"
 )
 
@@ -22,5 +23,9 @@ func Handlers(r *router.Router, db *sqlx.DB, log *zerolog.Logger) {
 
 	r.GET(fmt.Sprintf("/%s/source/{id}/campaigns", version), cache.Middleware(cacheDuration, sourcegrp.GetSourceCampaigns(&sgh), log))
 
-	r.GET("/debug/pprof/{profile:*}", pprofhandler.PprofHandler)
+	r.GET("/debug/pprof/", fasthttpadaptor.NewFastHTTPHandlerFunc(pprof.Index))
+	r.GET("/debug/pprof/cmdline", fasthttpadaptor.NewFastHTTPHandlerFunc(pprof.Cmdline))
+	r.GET("/debug/pprof/profile", fasthttpadaptor.NewFastHTTPHandlerFunc(pprof.Profile))
+	r.GET("/debug/pprof/symbol", fasthttpadaptor.NewFastHTTPHandlerFunc(pprof.Symbol))
+	r.GET("/debug/pprof/trace", fasthttpadaptor.NewFastHTTPHandlerFunc(pprof.Trace))
 }
